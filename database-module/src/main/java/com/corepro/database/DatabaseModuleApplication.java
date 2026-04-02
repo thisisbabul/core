e314e7dc7026b6ac55173180feb8d6749a60d0db
babul <babul.simec@gmail.com>
2026-04-02 18:53:36 +0600

    first commit

---
+++ database-module/src/main/java/com/corepro/database/DatabaseModuleApplication.java
@@ -0,0 +1,13 @@
+package com.corepro.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseModuleApplication.class, args);
+    }
+
+}
